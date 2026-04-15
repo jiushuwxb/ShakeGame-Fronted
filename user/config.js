ba9fcd99d6621effb5301a46e6ed32ba78d11363
王晓波 <17899316182@163.com>
2026-04-15 17:27:00 +0800

    部署

--- user/config.js
+++ user/config.js
@@ -1,6 +1,6 @@
 window.SHAKE_CONFIG = {
-  apiBaseUrl: 'https://www.shake0.fun',
-  wsUrl: 'wss://www.shake0.fun',
+  apiBaseUrl: 'https://fun.imc-china.com.cn',
+  wsUrl: 'wss://fun.imc-china.com.cn',
   questionnaireUrl: 'https://www.wjx.cn/',
   activityTitle: '希捷极速传输挑战赛',
   brandLine: 'Seagate Data Transfer Challenge'
